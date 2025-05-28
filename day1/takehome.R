36a8e5426f5e28474a26f5612688613b2ef841d5
VinhTranBa <Vinhtran.ytcc@gmail.com>
2025-05-28 08:09:24 +0700

    day 1 take home solution_Vinh

--- day1/takehome.R
+++ day1/takehome.R
@@ -21,9 +21,10 @@ covid_cases <- covid_cases %>%
 # Tỷ lệ phần trăm các trường hợp toàn cầu mà các trường hợp Trung Quốc báo cáo trong ngày
 covid_cases <- covid_cases %>%
   mutate(
-    percent_chn = (cases_CHN / case_global) * 100
+    percent_chn = (cases_chn / case_global) * 100
   )
 # Nhiệm vụ 3
+#
 compute_percent <- function(data, country_code) {
   country_col <- paste0("cases_", country_code)
   
@@ -42,3 +43,30 @@ compute_percent <- function(data, country_code) {
 }
 # ví dụ
 compute_percent(covid_cases, "vnm")
+
+
+# Tạo 3 bảng phần trăm riêng biệt cho từng quốc gia
+percent_vnm <- compute_percent(covid_cases, "vnm") %>%
+  select(date, percent_vnm = percent)
+
+percent_usa <- compute_percent(covid_cases, "usa") %>%
+  select(date, percent_usa = percent)
+
+percent_sgp <- compute_percent(covid_cases, "sgp") %>%
+  select(date, percent_sgp = percent)
+
+# Gộp các cột phần trăm trở lại bảng gốc
+covid_cases_with_percent <- covid_cases %>%
+  left_join(percent_vnm, by = "date") %>%
+  left_join(percent_usa, by = "date") %>%
+  left_join(percent_sgp, by = "date")
+
+#In bảng covid_cases cuối cùng
+covid_case_final <- covid_cases_with_percent %>% 
+  select(date, percent_chn, percent_vnm, percent_usa, percent_sgp)
+head(covid_case_final)
+
+# Nhiệm vụ 4
+skimr::skim(covid_case_final)
+# Nhận xét: Trung quốc có trung bình số ca cao nhất với 38.6 (sd:45.6), tiếp theo đến USA 12.3 (16.2), Việt Nam và Singapore có trung bình số ca thấp
+# tất cả đều missing 1 case tại ngày 20-1-2020
